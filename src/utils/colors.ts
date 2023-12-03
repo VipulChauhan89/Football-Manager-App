52b6a08b4eb3ae3cef90e3765cf4449d312a67c8
VipulChauhan89 <Vipulchauhan548@gmail.com>
2023-12-03 23:48:09 +0530

    Color configuration file to manage the color scheme across the application.

---
+++ src/utils/colors.ts
@@ -0,0 +1,24 @@
+export interface Color {
+  name: string;
+  value: string;
+}
+
+export const primaryColors: Color[] = [
+  { name: 'primary1', value: '#FEA013' },
+  { name: 'primary2', value: '#BA4A0C' },
+  { name: 'primary3', value: '#D23131' },
+];
+
+export const neutralColors: Color[] = [
+  { name: 'neutral1', value: '#222222' },
+  { name: 'neutral2', value: '#2D2D2D' },
+];
+
+export const textColors: Color[] = [
+  { name: 'text1', value: '#F8F8F8' },
+  { name: 'text2', value: '#CBCBCB' },
+  { name: 'text3', value: '#999999' },
+  { name: 'text4', value: '#707070' },
+];
+
+export const borderColor: string = '#494949';
